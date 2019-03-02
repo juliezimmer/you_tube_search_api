d7cdbcd37a1cd15fa368ebf3b5a86a8ad6942df0
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-02 14:24:56 -0600

    youtube search api working; returns 5 videos for search submitted

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,32 @@ import youtube from '../apis/youtube';
 
 // holds application state
 class App extends React.Component {
+   state = {
+      videos: [] // empty array is default value
+   }
+   
    // runs every time the search form is submitted
    // 'term' is what the user entered in the searchBar
-   // onTermSubmit is added to the SearchBar component in render as a prop
-   onTermSubmit = (term) => {
+   // onTermSubmit is added to the SearchBar component in render as a p   rop
+   onTermSubmit = async term => {
       // youtube function is called, which is a pre-configured instance of axios.
       // the route or path to where the get request is made goes in the parens.
       // accesses the search endpoint
       // 2nd argument is the params object with the search term
-      youtube.get('/search', {
+      const response = await youtube.get('/search', {
          params: {
             q: term
          }
       });
+      this.setState({ videos: response.data.items});
+      
    }; 
    
    render() {
       return (
          <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
+            I have {this.state.videos.length} videos.
          </div>
       );
    }
