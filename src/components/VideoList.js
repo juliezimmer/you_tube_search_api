c71e4891ad2cac50e5c4ba4abe9a4e1af0a838a5
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-02 21:01:59 -0600

    cleaned-up warnings in console; altered layout using semantic-ui grid system

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,8 +6,10 @@ const VideoList = ({ videos, onVideoSelect }) => {
    const renderedList = videos.map((video) => {
       return (
          <VideoItem 
+            key={video.id.videoId}
             onVideoSelect={onVideoSelect}
-            video={video} />
+            video={video} 
+         />
       );
    });
    
